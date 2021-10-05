6f6ae46ea90fd3cf927f2a37fb4ca03404c6978d
Guilherme Bernardo <bguilherme51@gmail.com>
2021-10-05 16:37:53 -0400

    atividade 4 finalizada

--- exercicios/lista4/questao02.hs
+++ exercicios/lista4/questao02.hs
@@ -1,6 +1,7 @@
 import Numeric (showHex, showIntAtBase)
 import Data.Char (intToDigit)
-
+ --lembrando que os numeros estão em string, logo no console deve-se ter "" entre eles
+ 
 bintodec :: Integral i => i -> i
 bintodec 0 = 0
 bintodec i = 2 * bintodec (div i 10) + (mod i 10)
@@ -23,14 +24,20 @@ hexChar ch
     | ch == 'D' = 13
     | ch == 'E' = 14
     | ch == 'F' = 15
+    | ch == 'a' = 10
+    | ch == 'b' = 11
+    | ch == 'c' = 12
+    | ch == 'd' = 13
+    | ch == 'e' = 14
+    | ch == 'f' = 15
     | otherwise     = 0
-
+ 
 parseHex :: String -> Integer
 parseHex [] = 0
 parseHex hxStr = hexChar (last hxStr) + (16 * parseHex (init hxStr))
-
+ 
 converter:: String -> String -> [String]
 converter x y
     |y == "bin" = [(show (bintodec (read x::Int))), (showHex (bintodec (read x::Int))(""))]--Num em Dec/Hex
     |y == "dec" = [(showHex (( read x::Int)) ("")), showIntAtBase (2) intToDigit (read x::Int) ("")]--Num em Hex/Bin
-    |y == "hex" = [show(parseHex x),show(parseHex x)]
+    |y == "hex" = [show(parseHex x),showIntAtBase (2) intToDigit (parseHex x) ("")]
---
+++ exercicios/lista4/questao05.hs
@@ -0,0 +1,6 @@
+fib 0 = 0 
+fib 1 = 1
+fib 2 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+restfib n m = (fib((fib n)) `mod` m)
